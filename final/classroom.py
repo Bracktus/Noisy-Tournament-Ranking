2e20fbdc26de308e228525a1938fb4c86da8ffd2
Bracktus <ricky@rchu.cc>
2023-02-17 19:12:59 +0000

    Factor out student generation

---
+++ final/classroom.py
@@ -0,0 +1,31 @@
+from random import gauss
+
+class Classroom():
+    def __init__(self, num_students, malicious=False):
+        self.num_students = num_students
+        self.grades = {s: self._gen_score() for s in range(self.num_students)}
+        self.malicious = malicious
+
+    def _gen_score(self):
+        """
+        Generates a score test between 0 and 1 along a gaussian distribution. With mean 0.5 and stdev 0.3
+        """
+        score = gauss(0.5, 0.2)
+        return max(0, min(score, 1))
+
+    def grader_skill(self, student):
+        """
+        Takes in a student number and return the probability of them marking a matchup correctly 
+        """
+        score = self.get_student_score(student)
+        if not self.malicious:
+            prob = score * 0.5 + 0.5
+        else:
+            prob = score
+
+        return prob
+
+    def get_student_score(self, student):
+        return self.grades[student]
+
+         
--- final/generate_tourney.py
+++ final/generate_tourney.py
@@ -1,53 +1,29 @@
-from random import gauss, random 
+from random import random 
 from collections import defaultdict
 
 class TournamentGenerator():
 
-    def __init__(self, assignments):
-        self.assignments = assignments
-        self.num_students = len(assignments)
+    def __init__(self, n, classroom):
+        self.num_students = n
 
-        self.grades = {}
         self.tournament_results = defaultdict(list)
-
         self.tournament_generated = False
+        self.classroom = classroom
 
-    def get_score(self):
-        """
-        Generates a score test between 0 and 1 along a gaussian distribution. With mean 0.5 and stdev 0.3
-        """
-        score = gauss(0.5, 0.3)
-        return max(0, min(score, 1))
-
- 
-    def get_prob_correct(self, score):
-        """
-        Converts a student's score into a probability of them grading a matchup correctly.
-        If they were purely guessing, then the probability would be 0.5.
-        Therefore, we can say the probability of them getting it correct would lie between [0.5 - 1].
-        Assuming they aren't malicious
-        """
-        return score * 0.5 + 0.5
-
-    def generate_tournament(self, malicious=False):
+    def generate_tournament(self, assignments):
         """
         This returns a dictionary. 
         The keys are the graders, the values are the results of the matches that the student marked.
         """
         # Each student is now assigned a score
-        self.grades = {s: self.get_score() for s in range(self.num_students)}
 
-        for grader in self.assignments:
-            matchups = self.assignments[grader]
-            grader_score = self.grades[grader]
-
-            if malicious:
-                grader_prob = grader_score
-            else:
-                grader_prob = self.get_prob_correct(grader_score)
+        for grader in assignments:
+            matchups = assignments[grader]
+            grades = self.classroom.grades
+            grader_prob = self.classroom.grader_skill(grader)
             
             for p1, p2 in matchups:
-                winner, loser = (p1, p2) if self.grades[p1] > self.grades[p2] else (p2, p1)
+                winner, loser = (p1, p2) if grades[p1] > grades[p2] else (p2, p1)
                 if grader_prob > random():
                     result = (winner, loser)
                 else:
@@ -56,20 +32,23 @@ class TournamentGenerator():
                 self.tournament_results[grader].append(result)
 
         self.tournament_generated = True
+        self.print_tournament()
 
     def print_tournament(self):
         if not self.tournament_generated:
             raise AttributeError("Tournament not generated")
             
+        grades = self.classroom.grades
         for student in self.tournament_results:
             matchups = self.tournament_results[student]
-            print(f"{student}: {matchups}: {self.grades[student]}")
+            print(f"{student}: {matchups}: {grades[student]}")
 
     def get_results(self):
         return self.tournament_results
 
     def get_true_ranking(self):
-        true_ranking = self.grades.items()
+        grades = self.classroom.grades
+        true_ranking = grades.items()
         true_ranking = sorted(true_ranking, key=lambda i: i[1], reverse=True)
         true_ranking = [student for (student, _) in true_ranking]
         return true_ranking
